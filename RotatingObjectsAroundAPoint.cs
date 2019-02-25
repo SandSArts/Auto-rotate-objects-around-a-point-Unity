5963a7d94ae86d789540c2b55609b20ec9404d42
SandS Arts <sandsarts.developer@gmail.com>
2019-02-25 03:30:35 -0800

    Add files via upload

---
+++ RotatingObjectsAroundAPoint.cs
@@ -0,0 +1,17 @@
+﻿//Coded by Shashank - Creator of SandS Arts , Mail me at sandsarts.developer@gmail.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObjectsAroundAPoint : MonoBehaviour {
+	
+	public float speed;                 //The speed of rotation
+	public Transform point;             //Point around which we want to rotate
+
+	void Update () {
+		// RotateAround is a function which helps us to rotate an object around a point
+		transform.RotateAround (point.position, Vector3.right, speed * Time.deltaTime);
+	
+	}
+}
